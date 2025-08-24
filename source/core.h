e775b9b3caf91de49e8d1aba34b32a7d77717b3d
Ryan Merritt <merrittaudio@gmail.com>
2025-08-24 16:53:03 -0400

    add which_side function for bezier cubic

--- source/core.h
+++ source/core.h
@@ -62,3 +62,83 @@ function F32 which_side_of_line(Vector2 a, Vector2 b, Vector2 p) {
   F32 side = a.x*b.y + a.y*p.x + b.x*p.y - b.y*p.x - a.y*b.x - a.x*p.y;
   return side;
 }
+
+
+
+
+/*
+  Figure out which "side" of a cubic bezier-line "p" is at. The implicit form of the bezier curve is taken from: https://www.mare.ee/indrek/misc/2d.pdf
+
+  I'm out of my depth here, but we basically need to get an implicit form of the bezier curve so that we can plug in an x and y and get back a scalar. Positive is one side of the curve, and negative is the other.
+
+  After testing this, you DO get good information about which side if you are close, but you also get some STRANGE results if you are far away from the curve. Basically, the curve is extended beyond the control points, so you will still need to do some other checks to use this functions effectively. Good luck!
+*/
+function F32 which_side_of_bezier(Vector2 first_point, Vector2 second_point, Vector2 first_control, Vector2 second_control, Vector2 test_point) {
+  // NOTE: The source text defined p0-p3 with control points as p1 and p2. Renaming to make copy-paste easier.
+  Vector2 p0 = first_point;
+  Vector2 p1 = first_control;
+  Vector2 p2 = second_control;
+  Vector2 p3 = second_point;
+
+  // NOTE: what are these coefficients?? :(
+  F32 a3 = p3.x - p0.x - 3.0f *  p2.x + 3.0f *  p1.x;
+  F32 a2 = -6.0f * p1.x + 3.0f *  p0.x + 3.0f *  p2.x;
+  F32 a1 = -3.0f * p0.x + 3.0f *  p1.x;
+  F32 a0 = p0.x;
+  F32 b3 = p3.y - p0.y - 3.0f *  p2.y + 3.0f *  p1.y;
+  F32 b2 = -6.0f * p1.y + 3.0f *  p0.y + 3.0f *  p2.y;
+  F32 b1 = -3.0f * p0.y + 3.0f *  p1.y;
+  F32 b0 = p0.y;
+
+  // NOTE: Sometimes you just need to trust random mathemeticians on the internet........
+  F32 v_xxx = b3 *b3 *b3;
+  F32 v_xxy = -3.0f*a3 *b3 *b3;
+  F32 v_xyy = 3.0f* b3 * a3 * a3;
+  F32 v_yyy = -a3 * a3 * a3;
+  F32 v_xx = -3.0f*a3 *b1*b2 *b3 + a1 *b2*b3 *b3 - a2 *b3 *b2 *b2
+    + 2.0f* a2 *b1 *b3*b3 + 3.0f* a3 *b0*b3 *b3 + a3 *b2*b2 *b2 - 3.0f* a0 *b3 *b3 *b3;
+  F32 v_xy = a1 * a3 *b2*b3 - a2 * a3 *b1*b3 - 6.0f* b0 *b3* a3 * a3
+    - 3.0f* a1 * a2 *b3*b3 - 2.0f* a2 * a3 *b2 *b2 + 2.0f* b2*b3 * a2 * a2
+    + 3.0f* b1*b2 * a3 * a3 + 6.0f* a0 * a3 *b3 *b3;
+  F32 v_yy = 3.0f* a1 * a2 * a3 *b3 + a3 *b2* a2 * a2 - a2 *b1* a3 * a3
+    - 3.0f* a0 *b3 * a3 * a3 - 2.0f* a1 *b2* a3 * a3 - b3 * a2 * a2 * a2 + 3.0f* b0* a3 * a3 * a3;
+  F32 v_x = a2 * a3 *b0*b1 *b3 - a1 * a2 *b1 *b2*b3 - a1 * a3 *b0*b2 *b3
+    + 6.0f* a0 * a3 *b1*b2 *b3 + b1 * a1 * a1 *b3*b3 + b3* a2 * a2 *b1 *b1
+    + 3.0f* b3* a3 * a3 *b0 *b0 + a1 * a3 *b1 *b2*b2 - a2 * a3 *b2*b1 *b1
+    - 6.0f* a0 * a3 *b0*b3 *b3 - 4.0f* a0 * a2 *b1 *b3 *b3 - 3.0f* b0 *b1*b2 * a3 * a3
+    - 2.0f* a0 * a1 *b2*b3 *b3 - 2.0f* a1 * a3 *b3 *b1 *b1 - 2.0f* b0 *b2*b3 * a2 * a2
+    + 2.0f* a0 * a2 *b3*b2 *b2 + 2.0f* a2 * a3 *b0 *b2 *b2 + 3.0f* a1 * a2 *b0 *b3*b3
+    + a3 * a3 *b1 *b1*b1 + 3.0f* a0 * a0 *b3 *b3*b3 - 2.0f* a0 * a3 *b2 *b2 *b2;
+  F32 v_y = a0 * a2 * a3 *b1 *b3 + a1 * a2 * a3 *b1*b2 - a0 * a1 * a3 *b2 *b3
+    - 6.0f* a1 * a2 * a3 *b0 *b3 - a1 * a1 * a1 *b3*b3 - 3.0f* a3 * a3 * a3 *b0 *b0
+    - a1 * a3 * a3 *b1*b1 - a3 * a1 * a1 *b2 *b2 - 3.0f* a3 * a0 * a0 *b3 *b3
+    + a2 *b2*b3 * a1 * a1 - a1 *b1 *b3* a2 * a2 - 3.0f* a0 *b1 *b2* a3 * a3
+    - 2.0f* a0 *b2 *b3* a2 * a2 - 2.0f* a3 *b0 *b2 * a2 * a2 + 2.0f* a0 * a2 * a3 *b2*b2
+    + 2.0f* a2 *b0 *b1* a3 * a3 + 2.0f* a3 *b1 *b3 * a1 * a1 + 3.0f* a0 * a1 * a2 *b3*b3
+    + 4.0f* a1 *b0 *b2* a3 * a3 + 6.0f* a0 *b0 *b3 * a3 * a3 + 2.0f* b0 *b3* a2 * a2 * a2;
+  F32 v_0 = a0 * a1 * a2 *b1 *b2*b3 + a0 * a1 * a3 *b0 *b2*b3 - a0 * a2 * a3 *b0 *b1 *b3
+- a1 * a2 * a3 *b0*b1 *b2 + b0 * a1 * a1 * a1 *b3 *b3 - b3 * a2 * a2 * a2 *b0 *b0
++ a1 *b0* a3 * a3 *b1 *b1 + a1 *b2* a0 * a0 *b3 *b3 + a3 *b0* a1 * a1 *b2 *b2
++ a3 *b2* a2 * a2 *b0 *b0 - a0 *b1* a1 * a1 *b3 *b3 - a0 *b3* a2 * a2 *b1 *b1
+- a2 *b1* a3 * a3 *b0 *b0 - a2 *b3* a0 * a0 *b2 *b2 - 3.0f* a0 *b3 * a3 * a3 *b0 *b0
+- 2.0f* a1 *b2 * a3 * a3 *b0*b0 + 2.0f* a2 *b1 * a0 * a0 *b3*b3
++ 3.0f* a3 *b0 * a0 * a0 *b3*b3 + a0 * a2 * a3 *b2 *b1*b1 + a1 *b0 *b1*b3 * a2 * a2
+- a0 * a1 * a3 *b1*b2 *b2 - a2 *b0*b2 *b3* a1 * a1 - 3.0f* a0 * a1 * a2 *b0 *b3 *b3
+- 3.0f* a3 *b1 *b2*b3 * a0 * a0 - 2.0f* a0 * a2 * a3 *b0 *b2*b2
+- 2.0f* a3 *b0 *b1*b3 * a1 * a1 + 2.0f* a0 * a1 * a3 *b3 *b1*b1
++ 2.0f* a0 *b0 *b2*b3 * a2 * a2 + 3.0f* a0 *b0 *b1 *b2 * a3 * a3
++ 3.0f* a1 * a2 * a3 *b3 *b0*b0 + a3 * a3 * a3 *b0 *b0*b0 - a0 * a0 * a0 *b3 *b3 *b3
++ a3 * a0 * a0 *b2*b2 *b2 - a0 * a3 * a3 *b1*b1 *b1;
+  // NOTE: Really should study up on this stuff and give better annotations...
+  F32 x = test_point.x;
+  F32 y = test_point.y;
+  F32 xx = x*x;
+  F32 yy = y*y;
+  F32 xxx = x*x*x;
+  F32 yyy = y*y*y;
+
+  // NOTE: Have faith
+  F32 side = v_xxx*xxx + v_xxy*xx*y + v_xyy*x*yy + v_yyy*yyy + v_xx*xx + v_xy*x*y + v_yy*yy + v_x*x + v_y*y + v_0;
+
+  return side;
+}
