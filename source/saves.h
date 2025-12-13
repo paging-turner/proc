3a5668d6e3573b1c285638ad16430dd8c29ae667
Ryan Merritt <merrittaudio@gmail.com>
2025-12-13 16:39:21 -0500

    sketch out new process serialization function

--- source/saves.h
+++ source/saves.h
@@ -1,12 +1,3 @@
-/*
-  The save file is just an array of process structs with a header.
-  Once we convert labels/text from char-buffers to strings/string-lists, we will also need to store string data.
-
-  If the Process struct is changed in a significant way, we will need to bump the version.
-
-  Ideally, we would also create functions for loading old versions of files... here's hoping.
-*/
-
 #define Save_File_Magic_Number AsciiID4('p', 'r', 'o', 'c')
 #define Save_File_Version 0
 
@@ -14,7 +5,10 @@ typedef struct {
   U32 magic_number;
   U32 version;
   U32 process_count;
-  U32 string_count;
+  union {
+    U32 string_count; // TODO: Once we phase out string_count, we can remove this member, and hopefully before we have enough v1 files to care.
+    U32 string_size;
+  };
 } Save_File_Header;
 
 typedef U64 Cold_Process_Id;
@@ -23,7 +17,7 @@ typedef U64 Cold_Process_Id;
 typedef struct {
   B32 flags;
   Vector2 position;
-  String_Chunk_List label;
+  String8 label;
 
   Cold_Process_Id in;
   Cold_Process_Id out;
@@ -46,16 +40,16 @@ Freeze_Member(Cold_Process, which_out   , 52);
 Freeze_Member(Cold_Process, unused_bytes, 56);
 
 
-#define Save_File_Size(process_count, string_count)\
+#define Save_File_Size_V1(process_count, string_size)\
   (sizeof(Save_File_Header)+\
    process_count*sizeof(Cold_Process)+\
-   string_count*sizeof(String_Chunk))
+   string_size)
 
-#define Save_File_Start_Of_Processes(save_file_bytes)\
+#define Save_File_Start_Of_Processes_V1(save_file_bytes)\
   (Cold_Process *)((save_file_bytes)+sizeof(Save_File_Header))
 
-#define Save_File_Start_Of_Strings(save_file_bytes, process_count)\
-  (String_Chunk *)((save_file_bytes)+(sizeof(Save_File_Header)+process_count*sizeof(Cold_Process)))
+#define Save_File_Start_Of_Strings_V1(save_file_bytes, process_count)\
+  (U8 *)((save_file_bytes)+(sizeof(Save_File_Header)+process_count*sizeof(Cold_Process)))
 
 //////////////////////////////////////
 // Saves Functions
@@ -71,7 +65,74 @@ function void set_as_current_file(Context *context, String_Chunk_List file_name)
   context->save_file_name = file_name;
 }
 
+function void write_save_file_v1(Context *context, Arena *arena, String_Chunk_List file_name) {
+  // TODO: ensure that the Saves_Filepath directory exists before writing a file into it.
+  os_set_current_directory(Saves_Filepath);
+
+  // save-file sizing and cold-indexing
+  U64 process_cold_index = 0;
+  U64 string_cold_size = 0;
+  U64 string_cold_offset = 0;
+  for (Process *p = context->processes.first; p != 0; p = p->next) {
+    p->cold_index = process_cold_index+1;
+    process_cold_index += 1;
+    for (String_Chunk *s = p->label.first; s != 0; s = s->next) {
+      string_cold_size += String_Chunk_Size; // TODO: At the last chunk, we can save space if the whole chunk is not used (if there are zeroes at the end).
+    }
+  }
+
+  String8 save_file_data;
+  save_file_data.size = Save_File_Size_V1(process_cold_index, string_cold_size);
+  save_file_data.str = arena_push(arena, save_file_data.size);
+
+  if (save_file_data.str) {
+    Save_File_Header *header = (Save_File_Header *)save_file_data.str;
+    header->magic_number = Save_File_Magic_Number;
+    header->version = 1;
+    header->process_count = process_cold_index;
+    header->string_size = string_cold_size;
+
+    Cold_Process *first_cold_process = Save_File_Start_Of_Processes_V1(save_file_data.str);
+    U8 *start_of_cold_string = Save_File_Start_Of_Strings_V1(save_file_data.str, process_cold_index);
+
+    process_cold_index = 0;
+    for (Process *p = context->processes.first; p != 0; p = p->next) {
+      Cold_Process *cold_process = first_cold_process + process_cold_index;
+
+      cold_process->flags = p->flags;
+      cold_process->position = p->position;
+      cold_process->label.str = start_of_cold_string + string_cold_offset;
+
+      if (p->in) {
+        cold_process->in = p->in->cold_index;
+      }
+      if (p->out) {
+        cold_process->out = p->out->cold_index;
+      }
+
+      cold_process->which_in = p->which_in;
+      cold_process->which_out = p->which_out;
+
+      // store string chunks
+      for (String_Chunk *s = p->label.first; s != 0; s = s->next) {
+        U8 *string_location = start_of_cold_string + string_cold_offset;
+        memory_move(string_location, s->str_array, String_Chunk_Size);
+        string_cold_offset += String_Chunk_Size;
+        cold_process->label.size += String_Chunk_Size;
+      }
+    }
+
+    set_as_current_file(context, file_name);
+  }
+}
+
+#if 1
 function void write_save_file(Context *context, Arena *arena, String_Chunk_List file_name) {
+  write_save_file_v1(context, arena, file_name);
+}
+#else
+function void write_save_file(Context *context, Arena *arena, String_Chunk_List file_name) {
+  Assert(!"TODO: Re-write this to use Cold_Process struct");
   // TODO: ensure that the Saves_Filepath directory exists before writing a file into it.
   os_set_current_directory(Saves_Filepath);
 
@@ -88,7 +149,7 @@ function void write_save_file(Context *context, Arena *arena, String_Chunk_List 
   }
 
   String8 save_file_data;
-  save_file_data.size = Save_File_Size(process_cold_index, string_cold_index);
+  save_file_data.size = Save_File_Size_V1(process_cold_index, string_cold_index);
   save_file_data.str = arena_push(arena, save_file_data.size);
 
   set_as_current_file(context, file_name);
@@ -98,8 +159,8 @@ function void write_save_file(Context *context, Arena *arena, String_Chunk_List 
     header->version = Save_File_Version;
     header->process_count = process_cold_index;
     header->string_count = string_cold_index;
-    Cold_Process *first_cold_process = Save_File_Start_Of_Processes(save_file_data.str);
-    String_Chunk *first_cold_string = Save_File_Start_Of_Strings(save_file_data.str, process_cold_index);
+    Cold_Process *first_cold_process = Save_File_Start_Of_Processes_V1(save_file_data.str);
+    String_Chunk *first_cold_string = Save_File_Start_Of_Strings_V1(save_file_data.str, process_cold_index);
 
     // copy processes to save file
     process_cold_index = 0;
@@ -109,7 +170,7 @@ function void write_save_file(Context *context, Arena *arena, String_Chunk_List 
 
       cold_process->flags = p->flags;
       cold_process->position = p->position;
-      cold_process->label = p->label;
+      /* cold_process->label = p->label; */
 
       /* cold_process->in = p->in; */
       /* cold_process->out = p->out; */
@@ -127,10 +188,10 @@ function void write_save_file(Context *context, Arena *arena, String_Chunk_List 
       }
 
       if (p->label.first) {
-        cold_process->label.first = PtrFromInt(p->label.first->cold_index);
+        /* cold_process->label.first = PtrFromInt(p->label.first->cold_index); */
       }
       if (p->label.last) {
-        cold_process->label.last = PtrFromInt(p->label.last->cold_index);
+        /* cold_process->label.last = PtrFromInt(p->label.last->cold_index); */
       }
 
       for (String_Chunk *s = p->label.first; s != 0; s = s->next) {
@@ -157,9 +218,27 @@ function void write_save_file(Context *context, Arena *arena, String_Chunk_List 
 
   os_set_current_directory(Build_Filepath);
 }
+#endif
 
 function void open_file_and_replace_processes_v1(Context *context, String_Chunk_List file_name_list) {
-  Assert(!"TODO");
+  os_set_current_directory(Saves_Filepath);
+  String8 file_name = string8_from_string_chunk_list(context->temp_arena, &file_name_list);
+  String8 file_data = os_file_read(context->temp_arena, file_name);
+
+  if (file_data.str && file_data.size > sizeof(Save_File_Header)) {
+    Save_File_Header *header = (Save_File_Header *)file_data.str;
+
+    if (header->magic_number == Save_File_Magic_Number) {
+      if (header->version == 1) {
+        Assert(!"TODO");
+      } else {
+        // TODO: Really, we should do some kind of switch here, to figure out the version and parse it accordingly.
+        printf("[ Error ] We only handle version 1 of the file right now, but this header declares a version of %d.\n", header->version);
+      }
+    } else {
+      printf("[ Error ] Expected save file magic number of 0x%x but got 0x%x instead.\n", Save_File_Magic_Number, header->magic_number);
+    }
+  }
 }
 
 #if 0
