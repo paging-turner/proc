bc0148efb65c6bc07f797659bc6e3a67f7ec22c0
Ryan Merritt <merrittaudio@gmail.com>
2025-12-31 07:22:00 -0500

    cruft

--- config/custom_keybinds_example.h
+++ config/custom_keybinds_example.h
@@ -1,11 +1,17 @@
 // Copy and edit a version of this file as "proc/config/custom_keybinds.h".
-Define_Keybind(Proc_Keybind_Def_Custom, CreateProcess,
+
+// TODO: Delete Define_Keybind_ and just change the args in Define_Keybind
+#define Define_Keybind_(bind_value, keybind_name, k, m, c, d)\
+  Define_Keybind(bind_value, keybind_name, k, m, c, d)
+
+
+Define_Keybind_(CreateProcess, Proc_Keybind_Def_Custom,
                Key_Kind_Mouse0, Modifier_Key_Shift,
                Ui_Constraint_NoHotProcess,
                "CUSTOM: Create a new process.");
 
 
-Define_Keybind(999, CreateProcess,
-               KEY_L, Modifier_Key_Shift,
-               Ui_Constraint_NoHotProcess,
-               "CUSTOM: Create a new process.");
+Define_Keybind_(CreateProcess, Proc_Keybind_Def_Custom,
+                KEY_L, Modifier_Key_Shift,
+                Ui_Constraint_NoHotProcess,
+                "CUSTOM: Create a new process.");
