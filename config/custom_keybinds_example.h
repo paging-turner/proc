ac36923fe3fb282f855ba5cfcce64f688807c243
Ryan Merritt <merrittaudio@gmail.com>
2025-12-31 07:04:13 -0500

    update keybind config example

--- config/custom_keybinds_example.h
+++ config/custom_keybinds_example.h
@@ -1,4 +1,11 @@
 // Copy and edit a version of this file as "proc/config/custom_keybinds.h".
-#ifdef Custom_Keybinds
-# include "../config/custom_keybinds.h"
-#endif
+Define_Keybind(Proc_Keybind_Def_Custom, CreateProcess,
+               Key_Kind_Mouse0, Modifier_Key_Shift,
+               Ui_Constraint_NoHotProcess,
+               "CUSTOM: Create a new process.");
+
+
+Define_Keybind(999, CreateProcess,
+               KEY_L, Modifier_Key_Shift,
+               Ui_Constraint_NoHotProcess,
+               "CUSTOM: Create a new process.");
